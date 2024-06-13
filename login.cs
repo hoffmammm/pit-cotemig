da18e73725e89e246b78cc3058f571f4ac15f981
Eduardo Hoffmam <eduhoffmam@gmail.com>
2024-06-13 18:37:30 -0300

    teste5

--- login.cs
+++ login.cs
@@ -17,23 +17,20 @@ namespace TelaDeLogin
         {
             string username = usernameTextBox.Text;
             string password = passwordTextBox.Text;
-            // Aqui você pode adicionar a lógica para autenticar o usuário
-            // Por exemplo, você pode verificar se as credenciais estão corretas no banco de dados
         }
 
         private void Signup(object sender, EventArgs e)
         {
-            MessageBox.Show("Implemente a funcionalidade de criar conta!");
+            CriarContaForm criarContaForm = new CriarContaForm();
+            criarContaForm.Show();
+            this.Hide();
         }
 
         private void ForgotPassword(object sender, EventArgs e)
         {
-            string email = Microsoft.VisualBasic.Interaction.InputBox("Digite seu e-mail para recuperar a senha:");
-            if (!string.IsNullOrEmpty(email))
-            {
-                // Aqui você pode adicionar a lógica para enviar um e-mail de recuperação de senha
-                MessageBox.Show("Um e-mail de recuperação foi enviado para " + email);
-            }
+            RecuperarSenhaForm recuperarSenhaForm = new RecuperarSenhaForm();
+            recuperarSenhaForm.Show();
+            this.Hide();
         }
     }
 }
